fa0370baa6b075200687bc0848a78b01763bc085
Pessinaaa <pedro.pessina99@gmail.com>
2022-09-05 23:16:04 -0300

    :memo::lipstick: About section

---
+++ Portfolio/src/about/about.jsx
@@ -0,0 +1,12 @@
+import CV from "../assets/CV Pedro Pessina.pdf"
+import './about.css'
+
+export default function About(){
+    return(
+        <div className="about">
+            <h2>Sobre mim</h2>
+            <p>Sou graduando em Desenvolvimento de Software Multiplataforma e atuo como <span>freelancer em desenvolvimento front-end</span>. Tenho como objetivo sempre adquirir e aprimorar conhecimentos.</p>
+            <a download href={CV}>Baixar currículo</a>
+        </div>
+    )
+}
--- Portfolio/src/app.jsx
+++ Portfolio/src/app.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Home from './home/home.jsx'
+import About from './about/about.jsx'
 
 export default function App(){
     return(
         <>
             <Home />
+            <About />
         </>
     )
 }
