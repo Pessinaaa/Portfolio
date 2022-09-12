3d0eff6ecc4ff3e9b84d7702c9b23ddaae77dbf1
Pessinaaa <pedro.pessina99@gmail.com>
2022-09-12 10:50:20 -0300

    :lipstick: Services section

--- Portfolio/src/services/services.jsx
+++ Portfolio/src/services/services.jsx
@@ -8,18 +8,18 @@ export default function Services(){
             <div>
                 <h3>Desenvolvimento Front-end</h3>
                 <ul className='services-list'>
-                    <li><FaCheck className="icon"/>Desenvolvimento de páginas web:</li>
+                    <li><i><FaCheck className="icon" /></i>Desenvolvimento de páginas web:</li>
                     <li>
                         <ul className='site-list'>
-                            <li>Landing page;</li>
-                            <li>One page;</li>
-                            <li>Institucional;</li>
-                            <li>Dinâmica.</li>
+                            <li>- Landing page;</li>
+                            <li>- One page;</li>
+                            <li>- Institucional;</li>
+                            <li>- Dinâmica.</li>
                         </ul>
                     </li>
-                    <li><FaCheck className="icon"/>Desenvolvimento de sites baseados em padrões de navegação.</li>
-                    <li><FaCheck className="icon"/>Adaptação das páginas para diferentes tamanhos de tela &#40;responsividade&#41;.</li>
-                    <li><FaCheck className="icon"/>Melhorias em sites já existentes.</li>
+                    <li><i><FaCheck className="icon" /></i>Desenvolvimento de sites baseados em padrões de navegação.</li>
+                    <li><i><FaCheck className="icon" /></i>Adaptação das páginas para diferentes tamanhos de tela &#40;responsividade&#41;.</li>
+                    <li><i><FaCheck className="icon" /></i>Melhorias em sites já existentes.</li>
                 </ul>
             </div>
         </div>
