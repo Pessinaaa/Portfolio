4bd490e3649136b79beefe36ec120c09ea0ed3ef
Pessinaaa <pedro.pessina99@gmail.com>
2023-05-25 09:29:56 -0300

    :lipstick: Alteração na lista de habilidades

--- Portfolio/src/skills/skills.jsx
+++ Portfolio/src/skills/skills.jsx
@@ -1,4 +1,5 @@
-import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaSass, FaPython, FaGitAlt, FaGithub, FaJava, FaNodeJs, FaDatabase, FaFigma } from "react-icons/fa"
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact, FaSass, FaPython, FaGitAlt, FaGithub, FaJava, FaNodeJs, FaDatabase, FaFigma, FaBootstrap, FaAngular } from "react-icons/fa"
+import { SiMongodb, SiSpring, SiTypescript, SiCsharp } from "react-icons/si"
 import './skills.css'
 
 export default function Skills(){
@@ -6,12 +7,18 @@ export default function Skills(){
         <div className='skills section'>
             <div className="skill-list">
                 <h2>Minhas habilidades</h2>
-                <ul>
+                <ul className="two-collumns">
                     <li><FaHtml5 className="icon"/>HTML5</li>
                     <li><FaCss3Alt className="icon"/>CSS3</li>
-                    <li><FaReact className="icon"/>React.js</li>
                     <li><FaSass className="icon"/>Sass</li>
-                    <li><FaPython className="icon"/>Python</li>
+                    <li><FaBootstrap className="icon"/>Bootstrap</li>
+                    <li><FaJava className="icon"/>Java</li>
+                    <li><SiSpring className="icon"/>Spring</li>
+                    <li><FaJsSquare className="icon"/>Javascript</li>
+                    <li><FaReact className="icon"/>React.js</li>
+                    <li><FaAngular className="icon"/>Angular</li>
+                    <li><FaDatabase className="icon"/>MySQL</li>
+                    <li><SiMongodb className="icon"/>MongoDB</li>
                     <li><FaGitAlt className="icon"/>Git</li>
                     <li><FaGithub className="icon"/>Github</li>
                 </ul>
@@ -19,10 +26,10 @@ export default function Skills(){
             <div className="skill-list">
                 <h2>Habilidades que estou desenvolvendo</h2>
                 <ul>
-                    <li><FaJava className="icon"/>Java</li>
-                    <li><FaJsSquare className="icon"/>Javascript</li>
+                    <li><FaPython className="icon"/>Python</li>
                     <li className="biggest"><FaNodeJs className="icon"/>Node.js</li>
-                    <li><FaDatabase className="icon"/>MySQL</li>
+                    <li><SiTypescript className="icon"/>TypeScript</li>
+                    <li><SiCsharp className="icon"/>C#</li>
                     <li><FaFigma className="icon"/>Figma</li>
                 </ul>
             </div>
