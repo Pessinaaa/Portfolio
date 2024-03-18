ad09393b40d2cbacb6d5503cb4b8caba93267e54
Pessinaaa <pedro.pessina99@gmail.com>
2024-03-18 10:55:10 -0300

    :construction: correções no carrossel

--- scripts/setup-owl.js
+++ scripts/setup-owl.js
@@ -1,26 +1,12 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items:4,
+    items:1,
     center:true,
     loop:true,
     margin:10,
-    autoHeight:true,
-    autoWidth:true,
     autoplay:true,
     autoplayTimeout:4000,
     autoplayHoverPause:true,
-    responsive:{
-        0:{
-            items:1,
-            autoWidth:false
-        },
-        600:{
-            items:2,
-        },
-        1000:{
-            items:4
-        }
-    }
 });
 $('.play').on('click',function(){
     owl.trigger('play.owl.autoplay',[1000])
